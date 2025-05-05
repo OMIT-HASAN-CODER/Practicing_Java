0559470e078e40adfdf8b0b36bbc37c6764b638b
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-05-06 01:07:19 +0600

    print ascii art

---
+++ src/com/chapter_1_problems/PrintAsciiArt.java
@@ -0,0 +1,12 @@
+package com.chapter_1_problems;
+
+public class PrintAsciiArt {
+    public static void main(String[] args) {
+        System.out.println("      - ");
+        System.out.println("     | |");
+        System.out.println("     | | __ ___   ____ _");
+        System.out.println(" _   | |/ _` \\ \\ / / _` |");
+        System.out.println("| |__| | (_| |\\ v / (_| |");
+        System.out.println(" \\____/ \\__,_| \\_/ \\__,_|");
+    }
+}
