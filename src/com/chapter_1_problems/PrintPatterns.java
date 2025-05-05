48439ff3fe942192f1ae74f4fcc520d98dddf361
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2025-05-06 00:52:00 +0600

    print patterns

---
+++ src/com/chapter_1_problems/PrintPatterns.java
@@ -0,0 +1,27 @@
+package com.chapter_1_problems;
+
+public class PrintPatterns {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+        System.out.println("* * * * *");
+
+        System.out.println("\n");
+
+        System.out.println("* * * * *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+
+        System.out.println("\n");
+
+        System.out.println("* * * * *");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+    }
+}
