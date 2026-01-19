3cd683b3ec3fdbd89ed0034450be30155ae85200
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-20 02:03:01 +0600

    solution of problem 04

---
+++ src/main/java/Chapter_01/Problem_04.java
@@ -0,0 +1,12 @@
+package Chapter_01;
+
+public class Problem_04 {
+    public static void main(String[] args) {
+        System.out.println("      _ ");
+        System.out.println("     | |");
+        System.out.println("     | | __ ___   ____ _");
+        System.out.println(" _   | |/ _` \\ \\ / / _` |");
+        System.out.println("| |__| | (_| |\\ V / (_| |");
+        System.out.println(" \\____/ \\__,_| \\_/ \\__,_|");
+    }
+}
