597496206ca0be33027972eac861a72d4856e65f
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-01-20 01:33:26 +0600

    solution of problem 03

---
+++ src/main/java/Chapter_01/Problem_03.java
@@ -0,0 +1,9 @@
+package Chapter_01;
+
+public class Problem_03 {
+    public static void main(String[] args) {
+        System.out.println("* * * * *\n* * * * *\n* * * * *\n* * * * *\n* * * * *");
+        System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *");
+        System.out.println("* * * * *\n *     * \n  *   *  \n   * *   \n    *    ");
+    }
+}
