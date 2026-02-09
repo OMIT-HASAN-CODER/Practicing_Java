0c42fac1ab8d9b50cdd41613b87562b7a968ef21
OMIT-HASAN-CODER <omithasan68479@gmail.com>
2026-02-09 23:54:17 +0600

    solution of problem 03

---
+++ src/main/java/Chapter_04/Problem_03.java
@@ -0,0 +1,13 @@
+package Chapter_04;
+
+import java.util.Arrays;
+
+public class Problem_03 {
+    public static void main(String[] args) {
+        int[] arr = {10, 1, 5, 2, 9, 15, 20, 14};
+
+        Arrays.sort(arr);
+
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+}
